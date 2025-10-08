2cf0dbfc3846734ee145de39f76e65ad4a045772
wemadeit2035 <amoswetu22@gmail.com>
2025-10-08 10:42:10 +0200

    tailwind.config.js for improved component detection

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,13 +1,18 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Add paths to all your components
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      // Add customizations here
       spacing: {
-        "4%": "4%", // For your px-[4%] class
+        "4%": "4%",
+      },
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
